c4f0e2b8dde1970e7478582d3028919202afed07
ly <ly@ly-PC>
2017-10-22 15:37:30 +0800

    login

--- src/main/java/com/supply/management/config/RootConfig.java
+++ src/main/java/com/supply/management/config/RootConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = {"com.supply.management"})
+@ComponentScan(basePackages = {"com.e.management"})
 //@ComponentScan(basePackages = {"com.ehai.springmvc"},
 //	excludeFilters={@Filter(type=FilterType.ANNOTATION, value=EnableWebMvc.class)
 //	})
